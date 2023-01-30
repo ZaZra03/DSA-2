dafefbdbfa9e3a6cf1d850e6e29e68847efbe47d
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-01-31 02:19:25 +0800

    Update Main.java

--- HashTable/Week1/Main.java
+++ HashTable/Week1/Main.java
@@ -2,57 +2,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// Main class that implements a menu-driven hash table program.
+// Main class to implement menu-driven hash table program.
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		// Initialize hash table with size 20.
 		HashTable hashTable = new HashTable(20);
-		// Buffered reader for user input.
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 		// Loop until user chooses to exit.
 		while (true) {
+			
+			// Display menu options.
+			System.out.println("\n1. Add New Key");
+			System.out.println("2. Display Hash Table");
+			System.out.println("3. Search");
+			System.out.println("4. Delete");
+			System.out.println("5. Exit");
+			System.out.print("\n>> ");
+
+			// Perform action based on user choice.
 			try {
-				// Display menu options.
-				System.out.println("\n1. Add New Key");
-				System.out.println("2. Display Hash Table");
-				System.out.println("3. Search");
-				System.out.println("4. Delete");
-				System.out.println("5. Exit");
-				System.out.print("\n>> ");
 				int choice = Integer.parseInt(in.readLine());
 				System.out.println();
 
-				// Perform action based on user choice.
 				switch (choice) {
 				case 1: // Add new key.
 					System.out.print("Enter a key: ");
 					int key = Integer.parseInt(in.readLine());
 
-					// Check if key is positive.
-					if (key < 0)
+					if (key < 0) {
 						System.out.println("\nKey should be a positive integer value.");
+						break;
+					}
 
 					hashTable.AddNewKey(key);
 					break;
 				case 2: // Display hash table.
 					hashTable.Display();
 					break;
-				case 3: // Search for a key.
+				case 3: // Search for key.
 					System.out.print("Enter a key to search: ");
 					key = Integer.parseInt(in.readLine());
 
-					if (key < 0)
+					if (key < 0) {
 						System.out.println("\nKey should be a positive integer value.");
+						break;
+					}
 
 					hashTable.Search(key);
 					break;
-				case 4: // Delete a key.
+				case 4: // Delete key.
 					System.out.print("Enter a key to delete: ");
 					key = Integer.parseInt(in.readLine());
 
-					if (key < 0)
+					if (key < 0) {
 						System.out.println("\nKey should be a positive integer value.");
+						break;
+					}
 
 					hashTable.Delete(key);
 					break;
@@ -63,7 +69,7 @@ public class Main {
 					System.out.println("Invalid choice!");
 				}
 			} catch (NumberFormatException e) {
-				System.out.println("\nError: Input must be an integer.");
+				System.out.println("\nInput must be an integer.");
 			}
 		}
 	}
