5df779f5305787577891f71b9979bc0b0bfe1f1c
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-02-18 00:36:31 +0800

    Update Node.java

--- HashTable/Week2/Node.java
+++ HashTable/Week2/Node.java
@@ -1,14 +1,32 @@
-package Week2;
+/**
+ * The Node class represents a node in a linked list. It contains an integer
+ * value (data) and a reference to the next node in the list (next).
+ * 
+ * @class Node
+ * 
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Hans Neil Emnacin
+ * @author Marc Ryzon Elomina
+ * @author Mark Cedrick De Vera
+ * @author Rizaldy Cantalejo
+ * 
+ * @see LinkedList
+ * @see HashTable
+ * @see Main
+ */
 
 public class Node {
+	// Class fields
 	private int data;
 	private Node next;
-	
+
+	// Constructor
 	public Node() {
 		data = 0;
 		next = null;
 	}
-	
+
 	public Node(int data) {
 		this.data = data;
 		next = null;
