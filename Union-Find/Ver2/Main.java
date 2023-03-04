4be53c53239d9369442293b1c7dc44d40f60e10e
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2023-03-04 10:32:03 +0800

    Added Comments.

--- Union-Find/Ver2/Main.java
+++ Union-Find/Ver2/Main.java
@@ -2,80 +2,104 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * The Main class contains the main method for the UnionFind program. It
+ * handles user input for adding nodes to a set and performing Union and Find
+ * operations on the set using the UnionFind class.
+ * 
+ * @class Main
+ * 
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Mark Cedrick De Vera
+ * 
+ * @see Node
+ * @see UnionFind
+ * @see LinkedList
+ */
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter 10 numbers to form a set:");
-		UnionFind u = new UnionFind(10);
-		int numCount = 0;
 
-		while (numCount < 10) {
-			try {
-				int x = Integer.parseInt(in.readLine());
-				u.addNode(new Node(x)); // add each number to the set
-				numCount++;
-			} catch (NumberFormatException e) {
-				System.out.println("\nInput must be an integer.");
-			}
-		}
+    /**
+     * The main method prompts the user to enter 10 numbers to form a set, then
+     * handles user input to perform Union and Find operations on the set using
+     * the UnionFind class.
+     * 
+     * @param args The command line arguments, not used in this program.
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter 10 numbers to form a set:");
+        UnionFind u = new UnionFind(10);
+        int numCount = 0;
 
-		while (true) {
-			System.out.println("\nMenu");
-			System.out.println("1. Union");
-			System.out.println("2. Find");
-			System.out.println("3. Exit");
-			System.out.print("\n>> ");
+        while (numCount < 10) {
+            try {
+                int x = Integer.parseInt(in.readLine());
+                u.addNode(new Node(x)); // add each number to the set
+                numCount++;
+            } catch (NumberFormatException e) {
+                System.out.println("\nInput must be an integer.");
+            }
+        }
 
-			try {
-				int choice = Integer.parseInt(in.readLine());
-				switch (choice) {
-				case 1:
-					System.out.print("\nEnter first number for Union operation: ");
-					int x = Integer.parseInt(in.readLine());
-					if (u.findNode(x).getData() != x) {
-						System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
-						break;
-					}
+        while (true) {
+            System.out.println("\nMenu");
+            System.out.println("1. Union");
+            System.out.println("2. Find");
+            System.out.println("3. Exit");
+            System.out.print("\n>> ");
 
-					System.out.print("\nEnter second number for Union operation: ");
-					int y = Integer.parseInt(in.readLine());
+            try {
+                int choice = Integer.parseInt(in.readLine());
+                switch (choice) {
+                    case 1:
+                        System.out.print("\nEnter first number for Union operation: ");
+                        int x = Integer.parseInt(in.readLine());
+                        if (u.findNode(x).getData() != x) {
+                            System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
+                            break;
+                        }
 
-					if (u.findNode(y).getData() != y) {
-						System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
-						break;
-					}
+                        System.out.print("\nEnter second number for Union operation: ");
+                        int y = Integer.parseInt(in.readLine());
 
-					u.union(u.findNode(x), u.findNode(y));
-					break;
-				case 2:
-					System.out.print("\nEnter first number for Find operation: ");
-					x = Integer.parseInt(in.readLine());
-					if (u.findNode(x).getData() != x) {
-						System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
-						break;
-					}
+                        if (u.findNode(y).getData() != y) {
+                            System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
+                            break;
+                        }
 
-					System.out.print("\nEnter second number for Find operation: ");
-					y = Integer.parseInt(in.readLine());
+                        u.union(u.findNode(x), u.findNode(y));
+                        break;
+                    case 2:
+                        System.out.print("\nEnter first number for Find operation: ");
+                        x = Integer.parseInt(in.readLine());
+                        if (u.findNode(x).getData() != x) {
+                            System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
+                            break;
+                        }
 
-					if (u.findNode(y).getData() != y) {
-						System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
-						break;
-					}
-					System.out.println();
-					System.out.println(u.connected(u.findNode(x), u.findNode(y)));
-					break;
-				case 3:
-					System.out.println("\nEnd of program...\n");
-					in.close();
-					return;
-				default:
-					System.out.println("\nInvalid choice. Please try again!");
-					break;
-				}
-			} catch (NumberFormatException e) {
-				System.out.println("\nInput must be an integer.");
-			}
-		}
-	}
+                        System.out.print("\nEnter second number for Find operation: ");
+                        y = Integer.parseInt(in.readLine());
+
+                        if (u.findNode(y).getData() != y) {
+                            System.out.println("\nERROR! That number is not part of the set. Please try again!\n");
+                            break;
+                        }
+                        System.out.println();
+                        System.out.println(u.connected(u.findNode(x), u.findNode(y)));
+                        break;
+                    case 3:
+                        System.out.println("\nEnd of program...\n");
+                        in.close();
+                        return;
+                    default:
+                        System.out.println("\nInvalid choice. Please try again!");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("\nInput must be an integer.");
+            }
+        }
+    }
 }
