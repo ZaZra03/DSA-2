7d4ff9365c87e8c97c42f84d4ab9fb180bc668b9
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2023-03-04 10:23:22 +0800

    Added Comments

--- Union-Find/Ver2/Node.java
+++ Union-Find/Ver2/Node.java
@@ -1,43 +1,59 @@
-class Node {
-	// Class fields
-	private int data;
-	private Node next;
-	private Node head;
-	private int index;
+/**
+ * Node class for a linked list in a disjoint-set data structure.
+ * 
+ * @class Node
+ * 
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Mark Cedrick De Vera
+ * @see Main
+ * @see UnionFind
+ * @see LinkedList
+ */
+public class Node {
+    // Class fields
+    private int data;
+    private Node next;
+    private Node head;
+    private int index;
 
-	// Constructor
-	public Node(int data) {
-		this.data = data;
-		this.setNext(null);
-	}
+    /**
+     * Constructor for Node class.
+     *
+     * @param data the integer data to store in the node
+     */
+    public Node(int data) {
+        this.data = data;
+        this.setNext(null);
+    }
 
-	// Getters
-	public int getData() {
-		return this.data;
-	}
+    // Getters
+    public int getData() {
+        return this.data;
+    }
 
-	public Node getHead() {
-		return this.head;
-	}
+    public Node getHead() {
+        return this.head;
+    }
 
-	public int getIndex() {
-		return this.index;
-	}
+    public int getIndex() {
+        return this.index;
+    }
 
-	public Node getNext() {
-		return next;
-	}
+    public Node getNext() {
+        return next;
+    }
 
-	// Setters
-	public void setHead(Node head) {
-		this.head = head;
-	}
+    // Setters
+    public void setHead(Node head) {
+        this.head = head;
+    }
 
-	public void setIndex(int x) {
-		this.index = x;
-	}
+    public void setIndex(int x) {
+        this.index = x;
+    }
 
-	public void setNext(Node next) {
-		this.next = next;
-	}
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
