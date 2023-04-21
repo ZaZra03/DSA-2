6f9d78a138f1eb6a08430ad5fd014a87e91380b3
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-04-21 15:10:23 +0800

    Update Node.java

--- LinkedList/persistent_linkedlist/Node.java
+++ LinkedList/persistent_linkedlist/Node.java
@@ -1,5 +1,3 @@
-package persistent_linkedlist;
-
 public class Node {
 	private int data;
 	private Node next;
@@ -9,28 +7,27 @@ public class Node {
 	private int index;
 	private boolean isDeleted;
 
-	
 	public Node() {
 		this.data = 0;
 		this.next = null;
 		this.prev = null;
 	}
-	
+
 	public Node(int data) {
 		this.data = data;
 		this.next = null;
 		this.prev = null;
 	}
-	
-    // Constructor to make a copy of a Node object
-    public Node(Node node) {
-        this.data = node.data;
-        this.index = node.index;
-        if (node.next != null) {
-            this.next = new Node(node.next);
-            this.next.prev = this; // update the prev field of the next node in the copied list
-        }
-    }
+
+	// Constructor to make a copy of a Node object
+	public Node(Node node) {
+		this.data = node.data;
+		this.index = node.index;
+		if (node.next != null) {
+			this.next = new Node(node.next);
+			this.next.prev = this; // update the prev field of the next node in the copied list
+		}
+	}
 
 	public int getData() {
 		return data;
@@ -39,19 +36,19 @@ public class Node {
 	public Node getNext() {
 		return next;
 	}
-	
+
 	public Node getPrev() {
 		return this.prev;
 	}
-	
+
 	public int getIndex() {
 		return index;
 	}
-	
+
 	public void setData(int data) {
 		this.data = data;
 	}
-	
+
 	public void setNext(Node next) {
 		this.next = next;
 	}
@@ -59,11 +56,10 @@ public class Node {
 	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
-	
+
 	public void setIndex(int index) {
 		this.index = index;
 	}
-	
 
 	public boolean isDeleted() {
 		return isDeleted;
