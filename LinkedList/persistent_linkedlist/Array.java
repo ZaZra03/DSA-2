7ee8f836afd32b621278178656d664b12ea7fed0
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-04-21 15:40:49 +0800

    Added comments

--- LinkedList/persistent_linkedlist/Array.java
+++ LinkedList/persistent_linkedlist/Array.java
@@ -1,15 +1,36 @@
+/**
+ * The Array class represents an array of Node objects. It has several methods
+ * for managing the array, such as adding a linked list to the array and
+ * increasing its capacity.
+ *
+ * @class Array
+ *
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Hans Neil Emnacin
+ * @author Marc Ryzon Elomina
+ * @author Mark Cedrick De Vera
+ * @author Rizaldy Cantalejo
+ * 
+ * @see Node
+ * @see LinkedList
+ * @see Main
+ */
 public class Array {
+	// Class fields
 	private Node array[];
 	private int size;
 	private int capacity;
 	private Node tail;
 
+	// Constructors
 	public Array(int size) {
 		this.setArray(new Node[size]);
 		this.setSize(size);
 		this.setCapacity(size);
 	}
 
+	// Getters and Setters
 	public Node[] getArray() {
 		return array;
 	}
@@ -34,6 +55,19 @@ public class Array {
 		this.capacity = capacity;
 	}
 
+	public Node getTail() {
+		return tail;
+	}
+
+	public void setTail(Node tail) {
+		this.tail = tail;
+	}
+
+	/**
+	 * Adds the specified linked list to this array.
+	 *
+	 * @param head the head node of the linked list to add to this array
+	 */
 	public void addLinkedList(Node head) {
 		if (capacity == capacity - size + 1)
 			increaseCapacity();
@@ -42,6 +76,10 @@ public class Array {
 		capacity--;
 	}
 
+	/**
+	 * Increases the capacity of the array by creating a new array with larger size
+	 * and copying the contents from the old array.
+	 */
 	public void increaseCapacity() {
 		Node temp[] = new Node[size + 10];
 		int index = 0;
@@ -50,13 +88,5 @@ public class Array {
 			index++;
 		}
 		this.array = temp;
-	}
-
-	public Node getTail() {
-		return tail;
-	}
-
-	public void setTail(Node tail) {
-		this.tail = tail;
 	}
 }
