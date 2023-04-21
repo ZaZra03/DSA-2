e9daa7f6289b63f62fad635dfafefef4e2ea39fb
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-04-21 15:39:46 +0800

    Added comments

--- LinkedList/persistent_linkedlist/LinkedList.java
+++ LinkedList/persistent_linkedlist/LinkedList.java
@@ -1,9 +1,31 @@
+/**
+ * The LinkedList class provides an implementation of a doubly linked list data
+ * structure. A linked list is a linear data structure that consists of nodes
+ * connected by links. This class contains methods for adding and deleting
+ * nodes, displaying the list, changing the value of a node, and viewing the
+ * history of the list.
+ *
+ * @class LinkedList 
+ * 
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Hans Neil Emnacin
+ * @author Marc Ryzon Elomina
+ * @author Mark Cedrick De Vera
+ * @author Rizaldy Cantalejo
+ * 
+ * @see Node
+ * @see Array
+ * @see Main
+ */
 public class LinkedList {
+	// Class fields
 	private Node head;
 	private Node tail;
 	private int size;
 	private Array history = new Array(10);
 
+	// Getters and Setters
 	public Node getHead() {
 		return this.head;
 	}
@@ -16,6 +38,11 @@ public class LinkedList {
 		this.head = head;
 	}
 
+	/**
+	 * Adds the specified node to the tail of this linked list.
+	 *
+	 * @param node the node to add to this linked list
+	 */
 	public void addNode(Node node) {
 		if (head == null) {
 			head = node;
@@ -37,6 +64,11 @@ public class LinkedList {
 		System.out.println("\nNode added successfully.\n");
 	}
 
+	/**
+	 * Deletes the node at the specified position in this linked list.
+	 *
+	 * @param index the position of the node to delete
+	 */
 	public void deleteNode(int index) {
 		if (size > 0) {
 			Node currentNode = head;
@@ -78,20 +110,25 @@ public class LinkedList {
 			System.out.println("\nList is empty\n.");
 	}
 
+	/**
+	 * Displays the values of all the existing nodes in this linked list.
+	 */
 	public void displayList() {
 		System.out.print("The values are: ");
-		Node currentNode = head;
-		while (currentNode != null) {
-			if (currentNode.isDeleted() == true) {
-				currentNode = currentNode.getNext();
-				continue;
+		for (Node currentNode = head; currentNode != null; currentNode = currentNode.getNext()) {
+			if (!currentNode.isDeleted()) {
+				System.out.print(currentNode.getData() + " ");
 			}
-			System.out.print(currentNode.getData() + " ");
-			currentNode = currentNode.getNext();
 		}
 		System.out.println("\n");
 	}
 
+	/**
+	 * Changes the value of the node at the specified position in this linked list.
+	 *
+	 * @param node  the new node to replace the existing node
+	 * @param index the position of the node to change
+	 */
 	public void changeValue(Node node, int index) {
 		Node currentNode = head;
 		while (currentNode.getIndex() != index) {
@@ -107,6 +144,13 @@ public class LinkedList {
 		node.setData(temp);
 	}
 
+	/**
+	 * Returns a string representation of the history of the node at the specified
+	 * position in this linked list.
+	 *
+	 * @param node the node to get the history of
+	 * @return a string representation of the history of the specified node
+	 */
 	public String nodeHistory(Node node) {
 		String temp = "";
 		int frontData = node.getData();
@@ -121,6 +165,10 @@ public class LinkedList {
 		return temp;
 	}
 
+	/**
+	 * Displays the values of all the nodes (including deleted nodes) in this linked
+	 * list.
+	 */
 	public void displayHistory() {
 		System.out.print("The values are: ");
 		Node currentNode = head;
@@ -131,6 +179,9 @@ public class LinkedList {
 		System.out.println("\n");
 	}
 
+	/**
+	 * Fixes the indices of the nodes in this linked list.
+	 */
 	public void fixIndex() {
 		Node currentNode = head;
 		int index = 1;
@@ -145,6 +196,13 @@ public class LinkedList {
 		}
 	}
 
+	/**
+	 * Returns the current value of the node at the specified position in this
+	 * linked list.
+	 *
+	 * @param index the position of the node to get the value of
+	 * @return the current value of the specified node
+	 */
 	public int showCurrentValue(int index) {
 		Node currentNode = head;
 		if (tail.getIndex() == index)
@@ -155,6 +213,12 @@ public class LinkedList {
 		return currentNode.getData();
 	}
 
+	/**
+	 * Returns the node at the specified position in this linked list.
+	 *
+	 * @param index the position of the node to get
+	 * @return the node at the specified position in this linked list
+	 */
 	public Node currentNode(int index) {
 		Node currentNode = head;
 		if (tail.getIndex() == index)
