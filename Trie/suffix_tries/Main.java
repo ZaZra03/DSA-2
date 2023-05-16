79af451f999ae104980d4a0da4a78b340ab49541
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-05-16 16:04:23 +0800

    Added toUpperCase() in line 71

--- Trie/suffix_tries/Main.java
+++ Trie/suffix_tries/Main.java
@@ -21,62 +21,60 @@ public class Main {
 //		trie.insert("sell");
 //		trie.insert("stock");
 //		trie.insert("stop");
-		
-		for(int i = 0; i<trie.getRoot().getChild().length; i++) {
-			System.out.println(trie.getRoot().getChild()[i].getData());
-		}
-		
+//
+//		for (int i = 0; i < trie.getRoot().getChild().length; i++) {
+//			System.out.println(trie.getRoot().getChild()[i].getData());
+//		}
+
 		System.out.print("How many strings? ");
 		value = Integer.parseInt(in.readLine());
 		System.out.println("Enter " + value + " strings:");
-		for(int i = 0; i < value; i++) {
+		for (int i = 0; i < value; i++) {
 			text = in.readLine();
 			trie.insert(text);
 		}
 		System.out.println("Standard trie created!");
 		System.out.println();
-		
-		while(true) {
+
+		while (true) {
 			String search = "";
 			Node temp = trie.getRoot();
 			System.out.print("Select child node from root node (" + trie.displayChild(temp) + "): ");
 			String character = in.readLine();
 			search += character + " ";
 			System.out.println("Display: " + search);
-			if(temp.getChild()[0].getData() == character.charAt(0)) temp = temp.getChild()[0];
-			else temp = temp.getChild()[1];
-			
-			while(!(temp.isChildEmpty())) {
+			if (temp.getChild()[0].getData() == character.charAt(0))
+				temp = temp.getChild()[0];
+			else
+				temp = temp.getChild()[1];
+
+			while (!(temp.isChildEmpty())) {
 				text = trie.displayChild(temp);
-				System.out.print("Select child node from "+ character + " (" + text + "): ");
+				System.out.print("Select child node from " + character + " (" + text + "): ");
 				character = in.readLine();
 				temp = temp.getChild()[trie.hashKey(character.charAt(0))];
 				text = trie.displayChild(temp);
-				if(text.length() != 1) {
+				if (text.length() != 1) {
 					search += character + " ";
 					System.out.println("Display: " + search);
 					temp = temp.getChild()[trie.hashKey(character.charAt(0))];
-					
-				} 
-				else {
+
+				} else {
 					search += character + " " + text + " ";
 					System.out.println("Display: " + search);
 					temp = temp.getChild()[trie.hashKey(text.charAt(0))];
 				}
 				System.out.println();
 			}
-			
+
 			System.out.print("Try again? ");
-			text = in.readLine();
-			
-			System.out.print("Try again? ");
-			text = in.readLine();
+			text = in.readLine().toUpperCase();
 			
 			switch (text) {
 			case "Y":
 				System.out.println();
 				break;
-			case "N": 
+			case "N":
 				System.out.println("Program will be terminated");
 				for (int i = 0; i < 3; i++) {
 					Thread.sleep(1000);
