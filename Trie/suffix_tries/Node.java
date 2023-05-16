6f657976cce8c5007a43bb34799a069008ce6a56
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-05-16 22:52:04 +0800

    Added comments

--- Trie/suffix_tries/Node.java
+++ Trie/suffix_tries/Node.java
@@ -1,20 +1,44 @@
-package suffix_tries;
 
+/**
+ * The Node class is a fundamental component of the SuffixTrie data structure.
+ * It represents a single node within the trie and contains information about
+ * the stored character, child nodes, and flags indicating whether the node is
+ * empty, representing the last character of a string, or both. The class
+ * provides constructors for creating nodes with or without a specified
+ * character and supports getter and setter methods for accessing and modifying
+ * the node's attributes. Nodes are utilized by the SuffixTrie class to
+ * construct the trie structure by connecting nodes as children of other nodes.
+ *
+ * @class Node
+ *
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Hans Neil Emnacin
+ * @author Marc Ryzon Elomina
+ * @author Mark Cedrick De Vera
+ * @author Rizaldy Cantalejo
+ * 
+ * @see SuffixTrie
+ * @see Main
+ */
 public class Node {
+	// Class fields
 	private char data;
 	private Node[] child;
 	private boolean isChildEmpty = true;
 	private boolean isLastCharacter;
-	
+
+	// Class constructors
 	public Node() {
 		setChild(new Node[2]);
 	}
-	
+
 	public Node(char data) {
 		this.setData(data);
 		setChild(new Node[26]);
 	}
 
+	// Getters and Setters
 	public char getData() {
 		return data;
 	}
