db2262bae5602f34667ded4bc6e9f5824b105abd
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2023-06-02 22:20:14 +0800

    Added comments

--- Sale-Inventory-UI/finals_lab_exam/Item.java
+++ Sale-Inventory-UI/finals_lab_exam/Item.java
@@ -1,13 +1,34 @@
-package finals_lab_exam;
-
+/**
+ * The Item class is a representation of an item in a Java application,
+ * containing important attributes such as itemID, itemPrice, itemStock,
+ * itemName, itemSold, and itemTotal. It includes a constructor that initializes
+ * its fields, as well as getter and setter methods for each attribute. The
+ * class serves as a model for an item, facilitating storage, manipulation, and
+ * use of the item's data in various business contexts such as inventory
+ * management or point-of-sale systems.
+ *
+ * @class Item
+ *
+ * @author Eugene Lawrence Autos
+ * @author Ezra Micah Malsi
+ * @author Hans Neil Emnacin
+ * @author Marc Ryzon Elomina
+ * @author Mark Cedrick De Vera
+ * @author Rizaldy Cantalejo
+ * 
+ * @see Array
+ * @see Main
+ * @see Transaction
+ */
 public class Item {
+	// Class fields
 	private int itemID;
 	private int itemPrice, itemStock;
 	private String itemName;
 	private int itemSold;
 	private int itemTotal;
-	
-	
+
+	// Constructor
 	public Item(int itemID, int itemPrice, int itemStock, String itemName) {
 		this.itemID = itemID;
 		this.setItemPrice(itemPrice);
@@ -16,6 +37,7 @@ public class Item {
 
 	}
 
+	// Getters and Setters
 	public int getItemID() {
 		return itemID;
 	}
